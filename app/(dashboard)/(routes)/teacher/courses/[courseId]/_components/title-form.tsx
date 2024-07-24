e29d81278b042b7eda1683b0f3b359b5fddac330
Brenn Aldwin A. Santiago <brennaldwin.santiago@gmail.com>
2024-07-24 21:04:30 +0800

    finish title form under course id

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,9 +1,15 @@
 "use client"
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod"
-
- 
+import { useRouter } from "next/navigation";
 
 interface TitleFormProps {
   initialData: {
@@ -20,6 +26,12 @@ const formSchema = z.object({
 
 export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
 
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current)
+
+  const router = useRouter()
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData
@@ -27,8 +39,71 @@ export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
 
   const { isSubmitting, isValid } = form.formState;
 
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+       await axios.patch(`api/courses/${courseId}`, values);
+       toast.success("Course updated!");
+       toggleEdit();
+       router.refresh();
+    } catch {
+      toast.error("Something went wrong.")
+    }
+  } 
+
 
   return (
-    <div>Title Form</div>
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Title
+        <Button variant="ghost" onClick={toggleEdit}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit title
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p className="text-sm mt-2">
+          {initialData.title}
+        </p>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField 
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input 
+                      disabled={isSubmitting}
+                      placeholder="e.g 'Advanced Web Development'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button 
+                disabled={!isValid || isSubmitting}
+                type="submit"
+              >
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
   )
 }
