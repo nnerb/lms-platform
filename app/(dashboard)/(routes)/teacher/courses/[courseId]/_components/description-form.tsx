0bb988106ce6f3d4fd56defe90d655df024a4066
Brenn Aldwin A. Santiago <brennaldwin.santiago@gmail.com>
2024-07-26 23:24:02 +0800

    remove uncessary import

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import { Pencil } from "lucide-react";
