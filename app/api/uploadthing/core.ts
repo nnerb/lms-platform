fa400d28c3e1a1cba91cb427f6d15594dcb09fff
Brenn Aldwin A. Santiago <brennaldwin.santiago@gmail.com>
2024-07-26 23:23:25 +0800

    add description form and image form

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,32 @@
+import { auth } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+ 
+const f = createUploadthing();
+ 
+// FileRouter for your app, can contain multiple FileRoutes
+
+const handleAuth = () => {
+  const { userId } = auth();
+
+  if (!userId) throw new Error("Unauthorized")
+
+  return { userId };
+}
+
+export const ourFileRouter = {
+  courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 }})
+  .middleware(() => handleAuth())
+  .onUploadComplete(() => {}),
+
+  courseAttachments: f(["text", "image", "video", "audio", "pdf"])
+  .middleware(() => handleAuth())
+  .onUploadComplete(() => {}),
+
+  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB"} })
+  .middleware(() => handleAuth())
+  .onUploadComplete(() => {})
+  
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof ourFileRouter;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,16 @@
 import type { Config } from "tailwindcss"
 
+import { withUt } from "uploadthing/tw";
+ 
+
 const config = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx,mdx}',
+    './src/**/*.{ts,tsx,mdx}',
+    './app/**/*.{ts,tsx,mdx}',
 	],
   prefix: "",
   theme: {
@@ -77,4 +81,4 @@ const config = {
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
 
-export default config
+export default withUt(config)
